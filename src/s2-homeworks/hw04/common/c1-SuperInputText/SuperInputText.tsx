003b6a38e5a2f260b2a7ac9d2fd559cb5533088b
ikomaz97 <ikomaz97@gmail.com>
2024-05-02 17:48:02 +0400

    02-05-24-3

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -1,77 +1,66 @@
-import React, {
-    ChangeEvent,
-    DetailedHTMLProps,
-    InputHTMLAttributes,
-    KeyboardEvent,
-    ReactNode,
-} from 'react'
-import s from './SuperInputText.module.css'
+import React, { ChangeEvent, KeyboardEvent } from 'react';
+import s from './SuperInputText.module.css';
 
-// тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement>
+type SuperInputTextProps = {
+    value: string;
+    onChange: (value: string) => void;
+    onChangeText?: (value: string) => void;
+    onEnter?: () => void;
+    error?: React.ReactNode;
+    spanClassName?: string;
+    className?: string;
+    id?: string;
+    // Другие пропсы для <input>, такие как placeholder, disabled и т.д.
+    // Для примера оставим только некоторые из них
+    placeholder?: string;
+    disabled?: boolean;
+};
 
-// здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
-// (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'type'> & {
-    // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeText?: (value: string) => void
-    onEnter?: () => void
-    error?: ReactNode
-    spanClassName?: string
-}
+const SuperInputText: React.FC<SuperInputTextProps> = ({
+                                                           value,
+                                                           onChange,
+                                                           onChangeText,
+                                                           onEnter,
+                                                           error,
+                                                           spanClassName,
+                                                           className,
+                                                           id,
+                                                           placeholder,
+                                                           disabled,
+                                                       }) => {
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e.currentTarget.value);
+        onChangeText?.(e.currentTarget.value);
+    };
 
-const SuperInputText: React.FC<SuperInputTextPropsType> = (
-    {
-        onChange,
-        onChangeText,
-        onKeyPress,
-        onEnter,
-        error,
-        className,
-        spanClassName,
-        id,
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && onEnter) {
+            onEnter();
+        }
+    };
 
-        ...restProps // все остальные пропсы попадут в объект restProps
-    }
-) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange?.(e) // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
-
-        onChangeText?.(e.currentTarget.value)
-    }
-    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-        onKeyPress?.(e)
-
-        onEnter && // если есть пропс onEnter
-        e.key === 'Enter' && // и если нажата кнопка Enter
-        onEnter() // то вызвать его
-    }
-
-    const finalSpanClassName = s.error
-        + (spanClassName ? ' ' + spanClassName : '')
-    const finalInputClassName = s.input
-        + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-        + (className ? ' ' + s.className : '') // задача на смешивание классов
+    const finalSpanClassName = s.error + (spanClassName ? ` ${spanClassName}` : '');
+    const finalInputClassName = s.input + (error ? ` ${s.errorInput}` : '') + (className ? ` ${className}` : '');
 
     return (
         <div className={s.inputWrapper}>
             <input
                 id={id}
-                type={'text'}
-                onChange={onChangeCallback}
-                onKeyPress={onKeyPressCallback}
+                type="text"
+                value={value}
+                onChange={onChangeHandler}
+                onKeyPress={onKeyPressHandler}
+                placeholder={placeholder}
+                disabled={disabled}
                 className={finalInputClassName}
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
-            <span
-                id={id ? id + '-span' : undefined}
-                className={finalSpanClassName}
-            >
-                {error}
-            </span>
+            {error && (
+                <span id={id ? `${id}-span` : undefined} className={finalSpanClassName}>
+                    {error}
+                </span>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default SuperInputText
+export default SuperInputText;
