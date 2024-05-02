952c80f738745728d05f542e24e86284bd3faf7c
ikomaz97 <ikomaz97@gmail.com>
2024-05-02 17:46:49 +0400

    02-05-24-2

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -1,101 +1,102 @@
-import React, { useState } from 'react'
-import s from './Stand.module.css'
-import SuperInputText from './common/c1-SuperInputText/SuperInputText'
-import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox'
-import SuperButton from './common/c2-SuperButton/SuperButton'
+import React, { useState } from 'react';
+import s from './Stand.module.css';
+import SuperInputText from './common/c1-SuperInputText/SuperInputText';
+import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox';
+import SuperButton from './common/c2-SuperButton/SuperButton';
 
 const Stand = () => {
-    const [stateForAllInputs, setValue] = useState<string>('')
-    const [error, setError] = useState<string>('')
+    const [stateForAllInputs, setStateForAllInputs] = useState('');
+    const [error, setError] = useState('');
 
-    const [stateForAllCheckboxes, setChecked] = useState<boolean>(false)
+    const [stateForAllCheckboxes, setStateForAllCheckboxes] = useState(false);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setStateForAllInputs(e.target.value);
+    };
+
+    const handleInputWithEnter = () => {
+        if (stateForAllInputs.trim() === '') {
+            setError('Error');
+        } else {
+            setError('');
+            setStateForAllInputs('');
+        }
+    };
+
+    const handleCheckboxChange = (checked: boolean) => {
+        setStateForAllCheckboxes(checked);
+    };
 
     return (
-        <div id={'hw4-stand'} className={s.stand}>
+        <div id="hw4-stand" className={s.stand}>
             <div className={s.inputs}>
-                {/*совместим со старым кодом:*/}
+                {/* Text input compatible with old code */}
                 <div>
                     <SuperInputText
                         id={'hw4-super-input-like-old'}
                         value={stateForAllInputs}
-                        onChange={(e) => setValue(e.currentTarget.value)}
+                        onChange={handleInputChange}
                     />
                 </div>
-                {/*инпут с ошибкой:*/}
+                {/* Text input with error handling */}
                 <div>
                     <SuperInputText
                         id={'hw4-super-input-with-error'}
                         value={stateForAllInputs}
-                        onChangeText={setValue}
+                        onChangeText={handleInputChange}
                         error={error}
-                        onEnter={() => {
-                            setError(
-                                stateForAllInputs.trim()
-                                    ? ''
-                                    : 'Error'
-                            )
-                            setValue('')
-                        }}
+                        onEnter={handleInputWithEnter}
                     />
                 </div>
             </div>
 
             <div className={s.buttons}>
-                {/*обычная кнопка:*/}
+                {/* Default button */}
                 <div>
-                    <SuperButton id={'hw4-super-button-default'}>
-                        default
-                    </SuperButton>
+                    <SuperButton id={'hw4-super-button-default'}>default</SuperButton>
                 </div>
-                {/*красная кнопка:*/}
+                {/* Red button */}
                 <div>
-                    <SuperButton id={'hw4-super-button-red'} xType={'red'}>
+                    <SuperButton id={'hw4-super-button-red'} xType="red">
                         red
                     </SuperButton>
                 </div>
-                {/*задизэйбленная кнопка:*/}
+                {/* Disabled red button */}
                 <div>
-                    <SuperButton
-                        id={'hw4-super-button-disabled'}
-                        xType={'red'}
-                        disabled
-                    >
+                    <SuperButton id={'hw4-super-button-disabled'} xType="red" disabled>
                         disabled
                     </SuperButton>
                 </div>
-                {/*задизэйбленная кнопка:*/}
+                {/* Secondary button */}
                 <div>
-                    <SuperButton
-                        id={'hw4-super-button-secondary'}
-                        xType={'secondary'}
-                    >
+                    <SuperButton id={'hw4-super-button-secondary'} xType="secondary">
                         secondary
                     </SuperButton>
                 </div>
             </div>
 
             <div className={s.checkboxes}>
-                {/*чекбокс с текстом:*/}
-                <div>
-                    <SuperCheckbox
-                        id={'hw4-super-checkbox-with-text'}
-                        checked={stateForAllCheckboxes}
-                        onChangeChecked={setChecked}
-                    >
-                        some text
-                    </SuperCheckbox>
-                </div>
-                {/*совместим со старым кодом:*/}
+                {/* Checkbox with text */}
                 <div>
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                        onChangeChecked={handleCheckboxChange}
+                    >
+                        some text
+                    </SuperCheckbox>
+                </div>
+                {/* Checkbox compatible with old code */}
+                <div>
+                    <SuperCheckbox
+                        id={'hw4-super-checkbox-like-old'}
+                        checked={stateForAllCheckboxes}
+                        onChange={(e) => handleCheckboxChange(e.currentTarget.checked)}
                     />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Stand
+export default Stand;
