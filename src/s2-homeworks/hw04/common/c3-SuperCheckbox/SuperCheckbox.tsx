cc459b4b910e889265365389e2384624c2bc2cf9
ikomaz97 <ikomaz97@gmail.com>
2024-05-08 18:12:46 +0400

    08-05-24-6

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,13 +27,13 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // задачка на написание онченджа
-        // ЕСЛИ(onChangeChecked ВООБЩЕ СУЩЕСТВУЕТ){
-        //    ПЕРЕДАТЬ ЕМУ ИВЕНТ.ЧТО-ТО.ЧТО-ТО
-        //}
-        // ЕСЛИ(onChange ВООБЩЕ СУЩЕСТВУЕТ){
-        //    ПЕРЕДАТЬ ЕМУ ИВЕНТ
-        //}
+        if (onChangeChecked) {
+            onChangeChecked(e.target.checked);
+        }
+
+        if (onChange) {
+            onChange(e);
+        }
     }
 
     const finalInputClassName = s.checkbox
