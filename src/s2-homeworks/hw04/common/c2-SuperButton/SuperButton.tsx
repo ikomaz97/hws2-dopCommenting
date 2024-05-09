3116bb9dd65a6790790fafd3e6ce942e28c13d48
ikomaz97 <ikomaz97@gmail.com>
2024-05-09 17:51:32 +0400

    09-05-24-35

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,11 +1,13 @@
-import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
-import s from './SuperButton.module.css';
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from './SuperButton.module.css'
 
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+// тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement>
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: 'КРАСНЫЙ' | 'secondary'; // Определяем тип xType как 'КРАСНЫЙ' или 'secondary'
-};
+    xType?: string
+}
 
 const SuperButton: React.FC<SuperButtonPropsType> = ({
                                                          xType,
@@ -13,18 +15,24 @@ const SuperButton: React.FC<SuperButtonPropsType> = ({
                                                          disabled,
                                                          ...restProps
                                                      }) => {
-    // Формируем строку классов finalClassName с использованием шаблонных строк
-    const finalClassName = `${s.button} 
-        ${xType === 'КРАСНЫЙ' ? s.red : xType === 'secondary' ? s.secondary : ''} 
-        ${disabled ? s.disabled : ''}`.trim(); // Удаляем лишние пробелы
+    const finalClassName = `${s.button}
+        ${disabled ? s.disabled : ''}
+        ${xType === 'red' ? s.red : ''}
+        ${xType === 'secondary' ? s.secondary : ''}
+        ${className ? ' ' + className : ''}`.trim(); // Убираем лишние пробелы и переносы строк
+
+
+    //ИЛИ ЕСЛИ НЕ ПОНЯТНО С finalClassName  ТОЖЕ САМОЕ ПРИ ПОМОЩИ ШАБЛОННЫХ СТРОК:
+    // `${s.СТИЛЬ КНОПКИ}  ${xType==='КРАСНЫЙ' ? ДАВАЙ КРАСНЫЙ СТИЛЬ : xType === 'secondary' ? ДАВАЙ СЕКОНДАРИ СТИЛЬ: ДАВАЙ ПО ДЕФОЛТУ } ${disabled ? ДАВАЙ ДИЗАБЛЕТ СТИЛЬ :  ПУСТУЮ СТРОКУ} `
+    // ЭТУ АЛХИМИЯ БУДЕМ ПОДРОБНО РАЗБИРАТЬ НА ДОПАХ
 
     return (
         <button
             disabled={disabled}
             className={finalClassName}
-            {...restProps}
+            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
         />
-    );
-};
+    )
+}
 
-export default SuperButton;
+export default SuperButton
