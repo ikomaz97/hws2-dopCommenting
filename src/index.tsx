761c63dc85110762e6a26725c3f1e1cf32113737
ikomaz97 <ikomaz97@gmail.com>
2024-06-29 19:00:08 +0400

    29-06-24-1

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,19 @@ import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 import store from './s2-homeworks/hw10/bll/store'
 import {Provider} from 'react-redux'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
         <Provider store={store}>
-            <App/>
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
         </Provider>
     </React.StrictMode>
 )
