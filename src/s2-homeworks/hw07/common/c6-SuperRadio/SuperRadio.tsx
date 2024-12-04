9372ceb25785f335cc08325768a62032bdeb61dd
ikomaz97 <ikomaz97@gmail.com>
2024-12-04 12:33:42 +0400

    03/12/2024/5

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -3,62 +3,61 @@ import React, {
     InputHTMLAttributes,
     DetailedHTMLProps,
     HTMLAttributes,
-} from 'react'
-import s from './SuperRadio.module.css'
+} from 'react';
+import s from './SuperRadio.module.css';
+
+type OptionType = { id: string | number; value: string };
 
 type DefaultRadioPropsType = DetailedHTMLProps<
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
->
-// тип пропсов обычного спана
+>;
+
 type DefaultSpanPropsType = DetailedHTMLProps<
     HTMLAttributes<HTMLSpanElement>,
     HTMLSpanElement
->
+>;
 
 type SuperRadioPropsType = Omit<DefaultRadioPropsType, 'type'> & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-
-    spanProps?: DefaultSpanPropsType // пропсы для спана
-}
+    options?: OptionType[];
+    onChangeOption?: (option: OptionType | null) => void; // Изменено для поддержки null
+    spanProps?: DefaultSpanPropsType;
+};
 
 const SuperRadio: React.FC<SuperRadioPropsType> = ({
-    id,
-    name,
-    className,
-    options,
-    value,
-    onChange,
-    onChangeOption,
-    spanProps,
-    ...restProps
-}) => {
+                                                       id,
+                                                       name,
+                                                       className,
+                                                       options,
+                                                       value,
+                                                       onChange,
+                                                       onChangeOption,
+                                                       spanProps,
+                                                       ...restProps
+                                                   }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        const selectedValue = e.target.value; // Получаем значение выбранной радио-кнопки
+        const selectedValue = e.target.value;
         if (onChangeOption && options) {
-            const selectedOption = options.find(o => o.id.toString() === selectedValue); // Ищем выбранный объект
-            onChangeOption(selectedOption); // Вызываем кастомный обработчик с выбранным объектом
+            const selectedOption = options.find((o: OptionType) => String(o.id) === selectedValue);
+            onChangeOption(selectedOption || null);
         }
-        if (onChange) {
-            onChange(e); // Вызываем стандартный onChange
-        }
+        if (onChange) onChange(e);
     };
 
-    const finalRadioClassName = s.radio + (className ? ' ' + className : '')
-    const spanClassName = s.span + (spanProps?.className ? ' ' + spanProps.className : '')
+    const finalRadioClassName = s.radio + (className ? ' ' + className : '');
+    const spanClassName = s.span + (spanProps?.className ? ' ' + spanProps.className : '');
 
-    const mappedOptions: any[] = options
-        ? options.map((o) => (
+    const mappedOptions: React.ReactNode = options
+        ? options.map((o: OptionType) => (
             <label key={name + '-' + o.id} className={s.label}>
                 <input
                     id={id + '-input-' + o.id}
                     className={finalRadioClassName}
-                    type={'radio'}
-                    name={name} // Все кнопки в группе должны иметь одинаковый name
-                    value={o.id} // Значение этой кнопки
-                    checked={o.id === value} // Отмечена ли эта кнопка
-                    onChange={onChangeCallback} // Обработчик события
+                    type="radio"
+                    name={name}
+                    value={o.id}
+                    checked={o.id === value}
+                    onChange={onChangeCallback}
                     {...restProps}
                 />
                 <span
@@ -66,13 +65,13 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                     {...spanProps}
                     className={spanClassName}
                 >
-                  {o.value}
-              </span>
+                      {o.value}
+                  </span>
             </label>
         ))
         : [];
 
-    return <div className={s.options}>{mappedOptions}</div>
-}
+    return <div className={s.options}>{mappedOptions}</div>;
+};
 
-export default SuperRadio
+export default SuperRadio;
