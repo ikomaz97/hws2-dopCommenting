857059aab2489feb79101abe230c1232ca2fc90a
ikomaz97 <ikomaz97@gmail.com>
2025-02-12 13:33:56 +0400

    06/02/25-7

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,58 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
-import { restoreState } from '../hw06/localStorage/localStorage';
-import s from './Clock.module.css';
+import React, { useState } from 'react'
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import { restoreState } from '../hw06/localStorage/localStorage'
+import s from './Clock.module.css'
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined);
-    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())));
-    const [show, setShow] = useState<boolean>(false);
+    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+    const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        if (timerId !== undefined) return;
-        const id = window.setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-        setTimerId(id);
-    };
+        if (!timerId) {
+            const id = window.setInterval(() => {
+                setDate(new Date())
+            }, 1000)
+            setTimerId(id)
+        }
+    }
 
     const stop = () => {
-        if (timerId !== undefined) {
-            clearInterval(timerId);
-            setTimerId(undefined);
+        if (timerId) {
+            clearInterval(timerId)
+            setTimerId(undefined)
         }
-    };
+    }
 
-    const onMouseEnter = () => {
-        setShow(true);
-    };
+    const onMouseEnter = () => setShow(true)
+    const onMouseLeave = () => setShow(false)
 
-    const onMouseLeave = () => {
-        setShow(false);
-    };
+    const stringTime = date.toLocaleTimeString('en-GB') // 24-hour format
+    const stringDate = date.toLocaleDateString('en-GB') // DD/MM/YYYY
 
-    useEffect(() => {
-        return () => {
-            if (timerId !== undefined) {
-                clearInterval(timerId);
-            }
-        };
-    }, [timerId]);
-
-    const stringTime = date.toLocaleTimeString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false,
-    });
-
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    const stringDate = `${day}.${month}.${year}`;
-
-    const stringDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
-    const stringMonth = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date);
+    const stringDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date)
+    const stringMonth = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date)
 
     return (
         <div className={s.clock}>
@@ -76,9 +55,7 @@ function Clock() {
                             <span id={'hw9-date'}>{stringDate}</span>
                         </>
                     ) : (
-                        <>
-                            <br />
-                        </>
+                        <br />
                     )}
                 </div>
             </div>
@@ -86,21 +63,21 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={timerId !== undefined}
+                    disabled={!!timerId}
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={timerId === undefined}
+                    disabled={!timerId}
                     onClick={stop}
                 >
                     stop
                 </SuperButton>
             </div>
         </div>
-    );
+    )
 }
 
-export default Clock;
+export default Clock
