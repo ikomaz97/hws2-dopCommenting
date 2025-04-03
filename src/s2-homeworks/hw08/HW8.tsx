d12e819300075e97b6a6064197b7e9cf04da47a0
ikomaz97 <ikomaz97@gmail.com>
2025-04-03 10:59:13 +0400

    03-04-25-3

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -1,91 +1,94 @@
-import React, { useState } from 'react';
-import { homeWorkReducer } from './bll/homeWorkReducer';
-import s from './HW8.module.css';
-import s2 from '../../s1-main/App.module.css';
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
-import User from './User';
+import React, { useState } from 'react'
+import { homeWorkReducer } from './bll/homeWorkReducer'
+import s from './HW8.module.css'
+import s2 from '../../s1-main/App.module.css'
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import User from './User'
 
 export type UserType = {
-    _id: number;
-    name: string;
-    age: number;
-};
+    _id: number
+    name: string
+    age: number
+}
 
 const initialPeople: UserType[] = [
-    { _id: 0, name: 'Кот', age: 3 },
-    { _id: 1, name: 'Александр', age: 66 },
-    { _id: 2, name: 'Коля', age: 16 },
-    { _id: 3, name: 'Виктор', age: 44 },
-    { _id: 4, name: 'Дмитрий', age: 40 },
-    { _id: 5, name: 'Ирина', age: 55 },
-];
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+]
 
 const HW8 = () => {
-    const [people, setPeople] = useState<UserType[]>(initialPeople);
-    const [currentSort, setCurrentSort] = useState<string>('');
+    const [people, setPeople] = useState<UserType[]>(initialPeople)
+    const [currentSort, setCurrentSort] = useState('')
 
-    const finalPeople = people.map((u: UserType) => <User key={u._id} u={u} />);
+    const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
 
     const sortUp = () => {
-        setPeople(homeWorkReducer(people, { type: 'sort', payload: 'up' }));
-        setCurrentSort('up');
-    };
+        setPeople(
+            homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
+        ) // Сортировка по имени в алфавитном порядке
+        setCurrentSort('up')
+    }
 
     const sortDown = () => {
-        setPeople(homeWorkReducer(people, { type: 'sort', payload: 'down' }));
-        setCurrentSort('down');
-    };
-
+        setPeople(
+            homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
+        ) // Сортировка по имени в обратном порядке
+        setCurrentSort('down')
+    }
     const check18 = () => {
-        setPeople(homeWorkReducer(people, { type: 'check', payload: 18 }));
-        setCurrentSort('18');
-    };
+        setPeople(
+            homeWorkReducer(initialPeople, {type: 'check', payload: 18})
+        ) // Совершеннолетние
+        setCurrentSort('18')
+    }
 
     return (
-        <div id="hw8">
+        <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #8</div>
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
                         <SuperButton
-                            id="hw8-button-up"
+                            id={'hw8-button-up'}
                             onClick={sortUp}
-                            disabled={currentSort === 'up'}
                             xType={currentSort === 'up' ? '' : 'secondary'}
                         >
                             Sort up
                         </SuperButton>
                         <SuperButton
-                            id="hw8-button-down"
+                            id={'hw8-button-down'}
                             onClick={sortDown}
-                            disabled={currentSort === 'down'}
                             xType={currentSort === 'down' ? '' : 'secondary'}
                         >
                             Sort down
                         </SuperButton>
                         <SuperButton
-                            id="hw8-button-18"
+                            id={'hw8-button-18'}
                             onClick={check18}
-                            disabled={currentSort === '18'}
                             xType={currentSort === '18' ? '' : 'secondary'}
                         >
                             Check 18+
                         </SuperButton>
                     </div>
 
-                    <table id="hw8-users" className={s.users}>
+                    <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
                         <tr>
                             <td className={s.nameCol}>Name</td>
                             <td className={s.ageCol}>Age</td>
                         </tr>
                         </thead>
+
                         <tbody>{finalPeople}</tbody>
                     </table>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default HW8;
+export default HW8
