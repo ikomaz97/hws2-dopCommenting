8c231917f2df138cb791660f5edf0b094088d189
ikomaz97 <ikomaz97@gmail.com>
2025-04-03 10:59:36 +0400

    03-04-25-6

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
-import store from './s2-homeworks/hw10/bll/store'
-import {Provider} from 'react-redux'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import { store } from './s2-homeworks/hw10/bll/store' // Именованный импорт
+import { Provider } from 'react-redux'
+import { DevSupport } from "@react-buddy/ide-toolbox"
+import { ComponentPreviews, useInitial } from "./dev"
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
         <Provider store={store}>
-            <DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
-            >
-                <App/>
+            <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+                <App />
             </DevSupport>
         </Provider>
     </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
