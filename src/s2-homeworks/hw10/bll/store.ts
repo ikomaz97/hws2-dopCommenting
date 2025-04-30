ba97951f32f86eb5b8c944ca6236935e63a4e18e
ikomaz97 <ikomaz97@gmail.com>
2025-04-30 16:26:49 +0400

    14-05-24-894

--- src/s2-homeworks/hw10/bll/store.ts
+++ src/s2-homeworks/hw10/bll/store.ts
@@ -1,17 +1,21 @@
-import { loadingReducer } from './loadingReducer'
-import { combineReducers, legacy_createStore } from 'redux'
-import { themeReducer } from '../../hw12/bll/themeReducer'
+import { combineReducers, legacy_createStore } from 'redux';
+import { loadingReducer } from './loadingReducer';
+import { themeReducer } from '../../hw12/bll/themeReducer';
 
 const reducers = combineReducers({
     loading: loadingReducer, // hw10
     theme: themeReducer, // hw12
-})
+});
 
-const store = legacy_createStore(reducers)
+const store = legacy_createStore(reducers);
 
-export default store
+export default store;
 
-export type AppStoreType = ReturnType<typeof reducers>
+// Типизация состояния приложения
+export type AppStoreType = ReturnType<typeof reducers>;
 
-// @ts-ignore
-window.store = store // for dev // для того чтобы автотесты видели состояние данных
+// Доступ к store в разработке для автотестов
+if (process.env.NODE_ENV === 'development') {
+    // @ts-ignore
+    window.store = store;
+}
