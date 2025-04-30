ee50e9207912aeb22fcbddbc04eefb4f97fb6bd7
ikomaz97 <ikomaz97@gmail.com>
2025-04-30 16:36:30 +0400

    14-05-25-80003

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,24 @@
-import s from './Loader.module.css'
+import React from 'react';
+import s from './Loader.module.css'; // подключаем стили
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+        <div className={s.loader}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                fill="none"
+                stroke="#3498db"
+            >
+                <circle cx="25" cy="25" r="20" strokeWidth="5" stroke="#f3f3f3" />
+                <path
+                    d="M25 5a20 20 0 1 1 0 40"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="5"
+                />
+            </svg>
+        </div>
+    );
+}
