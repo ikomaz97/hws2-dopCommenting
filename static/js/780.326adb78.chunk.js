898c77966dd65360a84c9fecb04859c660c7b08c
ikomaz97 <ikomaz97@gmail.com>
2025-05-08 14:29:48 +0400

    Updates

--- static/js/780.326adb78.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhws2=self.webpackChunkhws2||[]).push([[780],{780:(s,e,n)=>{n.r(e),n.d(e,{default:()=>j});var r=n(5043),c=n(5117),a=n(1614),h=n(579);const t=()=>(0,h.jsxs)(c.es,{children:[(0,h.jsx)(c.b7,{name:"App",children:(0,h.jsx)(c.uA,{name:"Loader",children:(0,h.jsx)(c.KT,{children:(0,h.jsx)(d,{})})})}),(0,h.jsx)(a.A,{})]});function d(){return(0,h.jsx)(r.Fragment,{children:"Loading..."})}const j=()=>(0,h.jsx)(c.m8,{palette:(0,h.jsx)(t,{})})}}]);
-//# sourceMappingURL=780.326adb78.chunk.js.map
