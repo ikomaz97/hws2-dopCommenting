de58ad2ee5d2cac538834a66d93ac2775c85d8fe
Chernov-V <ikomaz97@gmail.com>
2026-03-01 14:08:06 +0400

    212124

--- src/s2-homeworks/hw15/common/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/SuperPagination.tsx
@@ -1,90 +1,43 @@
 import React from 'react'
-import s from './SuperPagination.module.css'
 
-type SuperPaginationProps = {
+type Props = {
     page: number
     itemsCountForPage: number
     totalCount: number
     onChange: (page: number, count: number) => void
 }
 
-export const SuperPagination: React.FC<SuperPaginationProps> = ({
-                                                                    page,
-                                                                    itemsCountForPage,
-                                                                    totalCount,
-                                                                    onChange
-                                                                }) => {
+export const SuperPagination: React.FC<Props> = ({
+                                                     page,
+                                                     itemsCountForPage,
+                                                     totalCount,
+                                                     onChange
+                                                 }) => {
     const totalPages = Math.ceil(totalCount / itemsCountForPage)
 
-    // Создаем массив страниц для отображения
-    const getPagesArray = () => {
-        const pages = []
-        const maxVisiblePages = 7
-
-        if (totalPages <= maxVisiblePages) {
-            for (let i = 1; i <= totalPages; i++) {
-                pages.push(i)
-            }
-        } else {
-            if (page <= 4) {
-                for (let i = 1; i <= 5; i++) pages.push(i)
-                pages.push('...')
-                pages.push(totalPages)
-            } else if (page >= totalPages - 3) {
-                pages.push(1)
-                pages.push('...')
-                for (let i = totalPages - 4; i <= totalPages; i++) pages.push(i)
-            } else {
-                pages.push(1)
-                pages.push('...')
-                for (let i = page - 1; i <= page + 1; i++) pages.push(i)
-                pages.push('...')
-                pages.push(totalPages)
-            }
-        }
-        return pages
-    }
-
-    const handlePageChange = (newPage: number) => {
-        if (newPage >= 1 && newPage <= totalPages) {
-            onChange(newPage, itemsCountForPage)
-        }
-    }
-
-    const handleCountChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const newCount = Number(e.target.value)
-        onChange(1, newCount) // Сбрасываем на первую страницу при изменении количества
-    }
-
     return (
-        <div className={s.pagination}>
-            <div className={s.pages}>
-                {getPagesArray().map((p, index) => (
-                    <button
-                        key={index}
-                        className={`${s.pageButton} ${p === page ? s.active : ''} ${p === '...' ? s.dots : ''}`}
-                        onClick={() => typeof p === 'number' && handlePageChange(p)}
-                        disabled={p === '...'}
-                    >
-                        {p}
-                    </button>
-                ))}
-            </div>
-
-            <div className={s.showBlock}>
-                <span className={s.showText}>Показать</span>
-                <select
-                    className={s.select}
-                    value={itemsCountForPage}
-                    onChange={handleCountChange}
+        <div id="hw15-pagination">
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map(p => (
+                <button
+                    key={p}
+                    onClick={() => onChange(p, itemsCountForPage)}
+                    style={{ fontWeight: p === page ? 'bold' : 'normal' }}
                 >
-                    <option value="4">4</option>
-                    <option value="8">8</option>
-                    <option value="10">10</option>
-                    <option value="20">20</option>
-                </select>
-                <span className={s.showText}>строк в таблице</span>
-            </div>
+                    {p}
+                </button>
+            ))}
+
+            <select
+                id="hw15-pagination-select"
+                value={itemsCountForPage}
+                onChange={e => onChange(1, Number(e.target.value))}
+            >
+                <option value="4">4</option>
+                <option value="7">7</option>
+                <option value="10">10</option>
+            </select>
+
+            <span>строк в таблице</span>
         </div>
     )
 }
