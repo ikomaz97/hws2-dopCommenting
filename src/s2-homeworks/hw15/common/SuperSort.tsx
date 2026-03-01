f1f61e2c15e19693b728208c15773a7a6e4a2ec0
Chernov-V <ikomaz97@gmail.com>
2026-03-01 14:08:11 +0400

    212125

--- src/s2-homeworks/hw15/common/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/SuperSort.tsx
@@ -1,49 +1,45 @@
 import React from 'react'
-import s from './SuperSort.module.css'
+import { pureChange } from "../helpers/sortHelpers"
 
-type SuperSortProps = {
+type Props = {
     id?: string
     sort: string
     value: string
     onChange: (newSort: string) => void
 }
 
-export const SuperSort: React.FC<SuperSortProps> = ({
-                                                        sort,
-                                                        value,
-                                                        onChange,
-                                                        id
-                                                    }) => {
-    // Определяем направление сортировки для текущей колонки
-    const isActive = sort.includes(value)
-    const direction = isActive ? sort[0] : ''
+export const SuperSort: React.FC<Props> = ({
+                                               sort,
+                                               value,
+                                               onChange,
+                                               id
+                                           }) => {
+    const downSort = '0' + value
+    const upSort = '1' + value
 
     const handleClick = () => {
-        // pureChange логика:
-        // если сортировка не активна -> включаем сортировку по возрастанию (1'value')
-        // если активна по возрастанию -> меняем на убывание (0'value')
-        // если активна по убыванию -> отключаем сортировку ('')
-        if (!isActive) {
-            onChange(`1${value}`) // Включить сортировку по возрастанию
-        } else if (direction === '1') {
-            onChange(`0${value}`) // Поменять на убывание
-        } else {
-            onChange('') // Отключить сортировку
-        }
+        onChange(pureChange(sort, downSort))
+    }
+
+    const getIcon = () => {
+        if (sort === downSort) return '▼'
+        if (sort === upSort) return '▲'
+        return '↕️'
     }
 
     return (
         <span
             id={id}
-            className={`${s.sort} ${isActive ? s.active : ''}`}
             onClick={handleClick}
+            style={{
+                cursor: 'pointer',
+                marginLeft: 8,
+                display: 'inline-block',
+                minWidth: 20,
+                textAlign: 'center'
+            }}
         >
-            {value}
-            {isActive && (
-                <span className={s.icon}>
-                    {direction === '1' ? '▲' : '▼'}
-                </span>
-            )}
+            {getIcon()}
         </span>
     )
 }
