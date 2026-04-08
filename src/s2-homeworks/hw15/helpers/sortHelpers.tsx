086e4ac3f117cdf41dd83d35ad1187a759f076a1
Chernov-V <ikomaz97@gmail.com>
2026-04-09 00:09:41 +0400

    2121301

--- src/s2-homeworks/hw15/helpers/sortHelpers.tsx
+++ src/s2-homeworks/hw15/helpers/sortHelpers.tsx
@@ -1,14 +1,7 @@
 export const pureChange = (
-    sort: string,
-    down: string,
-    up: string
+  sort: string,
+  down: string,
+  up: string
 ) => {
-    switch (sort) {
-        case down:
-            return up
-        case up:
-            return ''
-        default:
-            return down
-    }
-}
+  return sort === down ? up : sort === up ? '' : down;
+};
