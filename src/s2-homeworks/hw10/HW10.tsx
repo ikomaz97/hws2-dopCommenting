a8307fc8ca8aee0b69a49b8d57c889e0eb0413ab
Chernov-V <ikomaz97@gmail.com>
2026-04-12 22:12:02 +0400

    11.04.2026.18

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { AppStoreType } from './bll/store'
-import { loadingAC } from './bll/loadingReducer'
+import {useDispatch, useSelector} from 'react-redux'
+import {AppStoreType} from './bll/store'
+import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
-import { Loader } from './Loader'
+import {Loader} from './Loader'
+
+/*
+* 1 - в файле loadingReducer.ts дописать типы и логику
+* 2 - получить isLoading из редакса
+* 3 - дописать функцию setLoading
+* 4 - сделать стили в соответствии с дизайном
+* */
 
 const HW10 = () => {
+    // useSelector, useDispatch // пишет студент
     const dispatch = useDispatch()
-    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
 
-    const setLoading = () => {
-        dispatch(loadingAC(true)) // Включаем загрузку
-
-        setTimeout(() => {
-            dispatch(loadingAC(false)) // Выключаем через 1.5 секунды
+    console.log(1, isLoading)
+    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+        // dispatch
+        // setTimeout
+        console.log(2, isLoading)
+        dispatch(loadingAC(true))
+        setTimeout(()=>{
+            dispatch(loadingAC(false))
+            console.log(3, isLoading)
         }, 1500)
     }
 
@@ -25,13 +37,12 @@ const HW10 = () => {
             <div className={s2.hw}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
-                        <Loader />
+                        <Loader/>
                     </div>
                 ) : (
                     <SuperButton
                         id={'hw10-button-start-loading'}
                         onClick={setLoading}
-                        disabled={isLoading} // Блокируем кнопку при загрузке
                     >
                         Set loading...
                     </SuperButton>
