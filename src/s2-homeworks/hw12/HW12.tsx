2c32e50864997f64363478ab1761f61909bf9388
Chernov-V <ikomaz97@gmail.com>
2026-04-12 22:12:18 +0400

    11.04.2026.20

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,32 +1,35 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
-import { useDispatch, useSelector } from 'react-redux'
-import { changeThemeId } from './bll/themeReducer'
-import { AppStoreType } from '../hw10/bll/store'
+import {useDispatch, useSelector} from 'react-redux'
+import {changeThemeId} from './bll/themeReducer'
 
-type OptionType = { id: string | number; value: string }
+/*
+* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
+* 2 - получить themeId из редакса
+* 3 - дописать тип и логику функции change
+* 4 - передать пропсы в SuperSelect
+* */
 
-const themes: OptionType[] = [
-    { id: 1, value: 'light' },
-    { id: 2, value: 'blue' },
-    { id: 3, value: 'dark' },
+const themes = [
+    {id: 1, value: 'light'},
+    {id: 2, value: 'blue'},
+    {id: 3, value: 'dark'},
 ]
 
 const HW12 = () => {
-    const dispatch = useDispatch()
-    const themeId = useSelector((state: AppStoreType) => state.theme.themeId)
+    // взять ид темы из редакса
 
-    const change = (option: OptionType | null) => {
-        if (option) {
-            // Преобразуем id в number
-            dispatch(changeThemeId(Number(option.id)))
-        }
+    const themeId = useSelector((state: any) => state.theme.themeId)
+    const dispatch = useDispatch()
+
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
-        document.documentElement.dataset.theme = themeId.toString()
+        document.documentElement.dataset.theme = themeId + ''
     }, [themeId])
 
     return (
@@ -39,8 +42,11 @@ const HW12 = () => {
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
+                    // сделать переключение тем
                     options={themes}
+                    value={themeId}
                     onChangeOption={change}
+
                 />
             </div>
         </div>
