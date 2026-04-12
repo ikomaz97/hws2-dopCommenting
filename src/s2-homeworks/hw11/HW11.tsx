e3c776c5b8baab4231a9b6a60df289073ce8c6be
Chernov-V <ikomaz97@gmail.com>
2026-04-12 22:12:11 +0400

    11.04.2026.19

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -3,16 +3,26 @@ import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import {isArray} from "node:util";
+
+/*
+* 1 - передать значения в оба слайдера
+* 2 - дописать типы и логику функции change
+* 3 - сделать стили в соответствии с дизайном
+* */
 
 function HW11() {
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 1))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: Event, value: number | number[]) => {
+    const change = (event: Event, value: unknown) => {
+        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
         if (Array.isArray(value)) {
             setValue1(value[0])
             setValue2(value[1])
-        } else {
+        }
+        if (typeof value === 'number') {
             setValue1(value)
         }
     }
@@ -20,6 +30,7 @@ function HW11() {
     return (
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
+
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
@@ -28,6 +39,8 @@ function HW11() {
                             id={'hw11-single-slider'}
                             value={value1}
                             onChange={change}
+                            // сделать так чтоб value1 изменялось // пишет студент
+
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -36,6 +49,8 @@ function HW11() {
                             id={'hw11-double-slider'}
                             value={[value1, value2]}
                             onChange={change}
+                            // сделать так чтоб value1/2 изменялось // пишет студент
+
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
@@ -46,6 +61,3 @@ function HW11() {
 }
 
 export default HW11
-
-
-
