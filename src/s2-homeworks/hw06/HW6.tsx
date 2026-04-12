bced94992cb7f70727ae3bf84026090789823944
Chernov-V <ikomaz97@gmail.com>
2026-04-12 22:10:45 +0400

    11.04.2026.13

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,36 +1,41 @@
-import React, { useState } from 'react';
-import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan';
-import { restoreState, saveState } from './localStorage/localStorage';
-import s2 from '../../s1-main/App.module.css';
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
-import s from './HW6.module.css';
+import React, { useState } from 'react'
+import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
+import { restoreState, saveState } from './localStorage/localStorage'
+import s2 from '../../s1-main/App.module.css'
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import s from './HW6.module.css'
+
+/*
+ * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
+ * 2 - дописать логику функции restore
+ * 3 - сделать стили в соответствии с дизайном
+ */
 
 const HW6 = () => {
-    // Инициализация value из localStorage
-    const [value, setValue] = useState<string>(() => restoreState<string>('hw6-editable-span-value', ''));
+    const [value, setValue] = useState<string>('')
 
     const save = () => {
-        saveState<string>('hw6-editable-span-value', value); // Сохраняем значение в localStorage
-    };
-
+        saveState<string>('hw6-editable-span-value', value)
+    }
     const restore = () => {
-        const restoredValue = restoreState<string>('hw6-editable-span-value', ''); // Восстанавливаем значение
-        setValue(restoredValue); // Обновляем состояние компонента
-    };
+        // делают студенты
+        setValue(restoreState('hw6-editable-span-value', value))
+    }
 
     return (
         <div id={'hw6'}>
             <div className={s2.hwTitle}>Homework #6</div>
 
+            {/*демонстрация возможностей компоненты:*/}
             <div className={s2.hw}>
                 <div className={s.editableSpanContainer}>
                     <SuperEditableSpan
                         id={'hw6-spanable-input'}
-                        value={value} // Привязка к состоянию value
-                        onChangeText={setValue} // Обновляем состояние при изменении текста
+                        value={value}
+                        onChangeText={setValue}
                         spanProps={{
                             id: 'hw6-editable-span',
-                            defaultText: 'enter text...', // Значение по умолчанию
+                            defaultText: 'enter text...',
                         }}
                     />
                 </div>
@@ -49,7 +54,7 @@ const HW6 = () => {
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default HW6;
+export default HW6
