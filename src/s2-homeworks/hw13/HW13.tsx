80453a4f0421a05235f0944ddbcbb0ae7e7977fd
Chernov-V <ikomaz97@gmail.com>
2026-04-13 15:42:38 +0400

    11.04.2026.22

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -1,107 +1,150 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s2 from '../../s1-main/App.module.css'
 import s from './HW13.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import axios from 'axios'
-
 import success200 from './images/200.svg'
 import error400 from './images/400.svg'
 import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
+
+/*
+* 1 - дописать функцию send
+* 2 - дизэйблить кнопки пока идёт запрос
+* 3 - сделать стили в соответствии с дизайном
+* */
 
 const HW13 = () => {
     const [code, setCode] = useState('')
     const [text, setText] = useState('')
     const [info, setInfo] = useState('')
     const [image, setImage] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
 
     const send = (x?: boolean | null) => () => {
         const url =
             x === null
-                ? 'https://xxxxxx.ccc'
+                ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
                 : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
         setCode('')
-        setText('')
         setImage('')
+        setText('')
         setInfo('...loading')
-        setIsLoading(true)
 
         axios
-            .post(url, { success: x })
+            .post(url, {success: x})
             .then((res) => {
-                setCode(String(res.status))
-                setImage(success200)
-                setText(res.data.errorText || res.data.info)
-                setInfo(res.data.info)
+
+                if (res.status === 200) {
+                    setCode('Код 200! Успех!');
+                    setText('...всё ок)')
+                    setImage(success200);
+                    setInfo('код 200 - обычно означает что скорее всего всё ок)')
+                } else if (res.data === null) {
+                  setCode('Ответ сервера: null');
+                  setImage(errorUnknown);
+                  setText('Error')
+                  setInfo('')
+                }
+                // дописать
+
             })
             .catch((e) => {
+                // дописать
                 if (e.response) {
-                    const status = e.response.status
+                    // Сервер ответил, но с ошибкой (4xx, 5xx)
+                    const status = e.response.status;
 
                     if (status === 400) {
-                        setCode('400')
-                        setImage(error400)
-                        setText(e.response.data.errorText)
-                        setInfo(e.response.data.info)
+                        setCode('Ошибка 400: Неверный запрос');
+                        setText('Ты не отправил success в body вообще!')
+                        setImage(error400);
+                        setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
                     } else if (status === 500) {
-                        setCode('500')
-                        setImage(error500)
-                        setText(e.response.data.errorText)
-                        setInfo(e.response.data.info)
-                    } else {
-                        setCode('Error')
-                        setImage(errorUnknown)
-                        setText(e.message)
-                        setInfo(e.name)
+                        setCode('Ошибка 500: Внутренняя ошибка сервера');
+                        setText('эмитация ошибки на сервере')
+                        setImage(error500);
+                        setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    }  else {
+                        setCode(`Ошибка ${status}: Неизвестная ошибка сервера`);
+                        setText('Error')
+                        setImage(errorUnknown);
+                        setInfo('Error')
                     }
                 } else {
-                    setCode('Error')
-                    setImage(errorUnknown)
-                    setText(e.message)
-                    setInfo('Network error')
+                    // Ошибка в настройке запроса (например, неверный URL)
+                    setCode('Axios Error: Ошибка при отправке запроса');
+                    setText('Error')
+                    setImage(errorUnknown);
+                    setInfo('Error')
                 }
             })
-            .finally(() => {
-                setIsLoading(false)
-            })
+
     }
 
     return (
-        <div id="hw13">
+        <div id={'hw13'}>
             <div className={s2.hwTitle}>Homework #13</div>
 
             <div className={s2.hw}>
-                {/* КНОПКИ */}
                 <div className={s.buttonsContainer}>
-                    <SuperButton id="hw13-send-true" onClick={send(true)} xType="secondary" disabled={isLoading}>
+                    <SuperButton
+                        id={'hw13-send-true'}
+                        onClick={send(true)}
+                        xType={'secondary'}
+                        // дописать
+                        disabled={info === '...loading'}
+
+                    >
                         Send true
                     </SuperButton>
+                    <SuperButton
+                        id={'hw13-send-false'}
+                        onClick={send(false)}
+                        xType={'secondary'}
+                        // дописать
+                        disabled={info === '...loading'}
 
-                    <SuperButton id="hw13-send-false" onClick={send(false)} xType="secondary" disabled={isLoading}>
+                    >
                         Send false
                     </SuperButton>
+                    <SuperButton
+                        id={'hw13-send-undefined'}
+                        onClick={send(undefined)}
+                        xType={'secondary'}
+                        // дописать
+                        disabled={info === '...loading'}
 
-                    <SuperButton id="hw13-send-undefined" onClick={send(undefined)} xType="secondary" disabled={isLoading}>
+                    >
                         Send undefined
                     </SuperButton>
+                    <SuperButton
+                        id={'hw13-send-null'}
+                        onClick={send(null)} // имитация запроса на не корректный адрес
+                        xType={'secondary'}
+                        // дописать
+                        disabled={info === '...loading'}
 
-                    <SuperButton id="hw13-send-null" onClick={send(null)} xType="secondary" disabled={isLoading}>
+                    >
                         Send null
                     </SuperButton>
                 </div>
 
-                {/* ОТВЕТ */}
                 <div className={s.responseContainer}>
                     <div className={s.imageContainer}>
-                        {image && <img src={image} className={s.image} alt="status" />}
+                        {image && <img src={image} className={s.image} alt="status"/>}
                     </div>
 
                     <div className={s.textContainer}>
-                        <div id="hw13-code" className={s.code}>{code}</div>
-                        <div id="hw13-text" className={s.text}>{text}</div>
-                        <div id="hw13-info" className={s.info}>{info}</div>
+                        <div id={'hw13-code'} className={s.code}>
+                            {code}
+                        </div>
+                        <div id={'hw13-text'} className={s.text}>
+                            {text}
+                        </div>
+                        <div id={'hw13-info'} className={s.info}>
+                            {info}
+                        </div>
                     </div>
                 </div>
             </div>
