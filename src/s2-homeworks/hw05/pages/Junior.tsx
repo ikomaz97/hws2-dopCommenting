adc3f9d8ce5cb60e46eb18a1247ffa80a69f73fb
Chernov-V <ikomaz97@gmail.com>
2026-04-13 15:43:50 +0400

    11.04.2026.30

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -6,13 +6,13 @@ import HW9 from '../../hw09/HW9'
 
 function Junior() {
     return (
-        <div id="hw5-page-junior">
+        <div id={'hw5-page-junior'}>
             <HW6 />
             <HW7 />
             <HW8 />
             <HW9 />
         </div>
-    );
+    )
 }
 
 export default Junior
